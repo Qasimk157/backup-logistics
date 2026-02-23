ee038ce8cc7e77c09c7b5f405f38c03f26c6ff7d
APKAYJWSJW7E7US7UC5Q <qasim@websential.ca>
2026-02-23 20:45:37 +0500

    dfgdf

--- src/common/http-common.ts
+++ src/common/http-common.ts
@@ -2,11 +2,11 @@ import axios, { AxiosResponse } from "axios";
 // import { LocalStorageService } from "../services/LocalStorageService";
 
 // export const baseURL = "http://192.168.1.4:8502/";
-// export const baseURL = "http://192.168.1.4:8502";
+export const baseURL = "http://192.168.1.4:8502";
 // export const baseURL = "http://192.168.1.4:8802";
 // export const baseURL = "http://192.168.1.4:8402";
 // export const baseURL = "http://localhost:8200";
-export const baseURL = "https://swv3.taxslips.com/"; //for live build
+// export const baseURL = "https://swv3.taxslips.com/"; //for live build
 // "homepage": "https://v3.taxslips.com/",      //this will paste on package.json file 
 
 const http = axios.create({
